8e24552fc7bf35cb4614660f426d836486d5afb6
JT <certainthings@gmail.com>
2019-01-10 11:45:15 +1100

    butterfly app skellington

---
+++ 07-databases/butterflies/butterflies.sql
@@ -0,0 +1,8 @@
+CREATE TABLE butterflies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  family TEXT,
+  image TEXT -- URL for a photo of a butterfly
+);
+
+INSERT INTO butterflies (name, family) VALUES ('Monarch', 'Nymphalidae');
