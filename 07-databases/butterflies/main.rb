50e08e8c1beb4658eb314ec6467e697bf1a5dbd9
JT <certainthings@gmail.com>
2019-01-10 14:48:41 +1100

    butterfly database pretty much done

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -8,10 +8,7 @@ end
 
 # INDEX - Show all butterflies
 get '/butterflies' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-  @butterflies = db.execute "SELECT * FROM butterflies"
-  db.close
+  @butterflies = query_db "SELECT * FROM butterflies"
   erb :butterflies_index
 end
 
@@ -22,26 +19,44 @@ end
 
 # CREATE - Add a new butterfly to the collection of butterflies
 post '/butterflies' do
-  db = SQLite3::Database.new 'database.sqlite3'
   query = "INSERT INTO butterflies (name, family, image) VALUES ('#{params["name"]}', '#{params["family"]}', '#{params["image"]}')"
-  db.execute query
-  db.close
+  query_db query
   redirect to('/butterflies') # Redirect will make a GET request
 end
 
 # SHOW - Show a single butterfly, based on its ID
 get '/butterflies/:id' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-  @butterfly = db.execute "SELECT * FROM butterflies WHERE id=#{params[:id]}"
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id=#{params[:id]}"
   @butterfly = @butterfly.first # Pluck the single butterfly from the results array.
-  db.close
   erb :butterflies_show
 end
 
+# EDIT - Show a form allowing editing of a particular butterfly
+get '/butterflies/:id/edit' do
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+  @butterfly = @butterfly.first # Get the butterfly from inside the array of results
+  erb :butterflies_edit
+end
+
+post '/butterflies/:id' do
+  query = "UPDATE butterflies SET name='#{params[:name]}', family='#{params[:family]}', image='#{params[:image]}' WHERE id=#{ params[:id] }"
+  query_db query
+  redirect to("/butterflies/#{params[:id]}")
+end
+
+# DELETE - Delete a particular butterfly from the DB, based on its ID
 get '/butterflies/:id/delete' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.execute "DELETE FROM butterflies WHERE id=#{ params["id"] }"
-  db.close
+  query_db "DELETE FROM butterflies WHERE id=#{ params[:id] }"
   redirect to("/butterflies")
+end
+
+def query_db(sql_statement)
+  puts sql_statement # Optional feature which is nice for debugging
+
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+  results = db.execute sql_statement
+  db.close
+
+  results # Implicit return
 end
