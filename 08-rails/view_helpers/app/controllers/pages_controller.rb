cbb730dc75509e1bbb95ea42a9cb82a6cbdc2c61
JT <certainthings@gmail.com>
2019-01-15 10:54:18 +1100

    view helpers for numbers and text

---
+++ 08-rails/view_helpers/app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  def home
+    # Take advantage of implicit rendering -- a view with the same name
+  end
+
+  def numbers
+    @amount = 9234.248
+    @large_number = 234834873483
+    @phone = 2125551212
+  end
+
+  def text
+    @numbers = (1..20).to_a
+    @friend_count = 1
+    @enemy_count = 2
+    @story = "A very very very very very very very long time ago in a galaxy far far far far far away" * 80
+  end
+end
