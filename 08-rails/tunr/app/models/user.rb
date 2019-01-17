f15f373604ae60032d7f71ccb66a3e0731b79b7b
JT <certainthings@gmail.com>
2019-01-17 15:30:16 +1100

    authorisation and admin examples

--- 08-rails/tunr/app/models/user.rb
+++ 08-rails/tunr/app/models/user.rb
@@ -2,13 +2,16 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  email      :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  email           :text
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :text
 #
 
 class User < ApplicationRecord
   has_secure_password
+  # Validations: see the Rails Guide
+  validates :email, :presence => true, :uniqueness => true
   has_many :mixtapes
 end
