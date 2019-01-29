88e7e6d3c4bf4624d79a29a448210cf36ec9020f
JT <certainthings@gmail.com>
2019-01-29 12:09:57 +1100

    rspec bank complete

--- 09-tdd/bank/bank.rb
+++ 09-tdd/bank/bank.rb
@@ -1,7 +1,24 @@
 class Bank
-  attr_reader :name
+  attr_reader :name, :accounts
 
   def initialize(name)
     @name = name
+    @accounts = Hash.new(0) # 0 as default, instead of nil
+  end
+
+  def create_account(account_name, deposit)
+    @accounts[account_name] = deposit
+  end
+
+  def deposit(account_name, amount)
+    @accounts[account_name] += amount
+  end
+
+  def withdraw(account_name, amount)
+    @accounts[account_name] -= amount if (amount <= @accounts[account_name])
+  end
+
+  def balance(account_name)
+    @accounts[account_name]
   end
 end
--- 09-tdd/bank/spec/bank_spec.rb
+++ 09-tdd/bank/spec/bank_spec.rb
@@ -1,23 +1,52 @@
 require_relative '../bank'
 
 describe Bank do
+  let(:bank) { Bank.new 'TDD Bank' }
+  # "." denotes a class method
   describe '.new' do
     it "creates a new bank object" do
-      bank = Bank.new 'TDD Bank'
       expect(bank).to_not be nil
     end
 
     it "assigns a bank a name" do
-      bank = Bank.new 'TDD Bank'
       expect(bank.name).to eq 'TDD Bank'
     end
   end
 
+  # "#" denotes an instance method
   describe '#create_account' do
     it "creates an account for a particular person" do
-      bank = Bank.new 'TDD Bank'
       bank.create_account 'Craigsy', 7
       expect(bank.accounts['Craigsy']).to eq 7
+    end
+  end
+
+  describe '#deposit' do
+    it "deposits an amount into a particular account" do
+      bank.create_account 'Jonesy', 200
+      bank.deposit 'Jonesy', 300
+      expect(bank.accounts['Jonesy']).to eq 200+300
+    end
+  end
+
+  describe '#withdraw' do
+    it 'withdraws an amount from a particular account' do
+      bank.create_account 'Bazza', 200
+      bank.withdraw 'Bazza', 50
+      expect(bank.accounts['Bazza']).to eq 200-50
+    end
+
+    it 'ignores withdrawals that exceed the balance' do
+      bank.create_account 'Charlie', 1
+      bank.withdraw 'Charlie', 1_000_000
+      expect(bank.balance 'Charlie').to eq 1
+    end
+  end
+
+  describe '#balance' do
+    it 'returns the balance for some particular account' do
+      bank.create_account 'Mad Robby', 500
+      expect(bank.balance 'Mad Robby').to eq 500
     end
   end
 end
