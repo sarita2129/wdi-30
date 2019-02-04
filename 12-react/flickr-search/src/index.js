b21cc3fe64c5dbe32c072e1dd9bec518dc969468
JT <certainthings@gmail.com>
2019-02-04 12:20:55 +1100

    flickr search skellington with preact and webpack

---
+++ 12-react/flickr-search/src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'; // hehehe it's really preact
+import ReactDOM from 'react-dom'; // hehehe it's really preact
+import 'preact/devtools';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.body);
