3d6dd0f7b50449bd152c967d9dc03bcd82fcdbb9
JT <certainthings@gmail.com>
2019-02-04 14:59:01 +1100

    flickr search in preact

--- 12-react/flickr-search/src/components/FlickrSearch.js
+++ 12-react/flickr-search/src/components/FlickrSearch.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
+import jsonp from 'jsonp-es6'; // IRL: we'd use Axios for regular AJAX (see tomorrow)
 
 class FlickrSearch extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [] // Whenever these change <Gallery /> will rerender (because Gallery's props will change)
+    };
+    this.fetchImages = this.fetchImages.bind(this);
+  };
+
+  // This is kinda an event handler but it's not being called by a user interface event.
+  fetchImages(q) {
+    console.log('Searching Flickr for', q);
+    const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?';
+    const flickrParams = {
+      method: 'flickr.photos.search',
+      api_key: '2f5ac274ecfac5a455f38745704ad084',
+      text: q,
+      format: 'json'
+    };
+
+    const generateURL = function (p) {
+      return [
+        'http://farm',
+        p.farm,
+        '.static.flickr.com/',
+        p.server,
+        '/',
+        p.id,
+        '_',
+        p.secret,
+        '_q.jpg' // Change 'q' to something else from the docs for different sizes
+      ].join('');
+    };
+
+    // This actually initiates the request to Flick for images matching the term `q`.
+    // In real life we'd use `axios` or `fetch` to make this request.
+    jsonp(flickrURL, flickrParams, {callback: 'jsoncallback'}).then((results) => {
+      const images = results.photos.photo.map(generateURL);
+      this.setState({images}); // {"images": images}
+    });
+
+  }
+
   render() {
     return (
       <div>
-        <SearchForm />
-        <Gallery />
+      {/*
+        The parent FlickrSearch can't interact with the child (Gallery)'s state directly.
+        But it CAN pass in a callback for the child to call/invoke/run at the appropriate time,
+        and that callback can access the state of the child from the parent's scope.
+      */}
+        <SearchForm onSubmit={ this.fetchImages } />
+        <Gallery images={ this.state.images }/>
       </div>
     );
   }
@@ -16,6 +64,7 @@ class SearchForm extends Component {
     super();
     this.state = {query: ''};
     this._handleInput = this._handleInput.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
   }
 
   _handleInput(e) {
@@ -24,7 +73,9 @@ class SearchForm extends Component {
 
   _handleSubmit(e) {
     e.preventDefault();
-    console.log('submit');
+    // We "handle" the submit by running whatever function the parent passed to us as onSubmit.
+    // And we pass in the thing the parent probably cares about, in this case: the query.
+    this.props.onSubmit(this.state.query);
   }
 
   render() {
@@ -40,9 +91,18 @@ class SearchForm extends Component {
 class Gallery extends Component {
   render() {
     return (
-      <h2>Gallery</h2>
+      <div>
+        { this.props.images.map( (url) => <Image url={url} /> ) }
+      </div>
     );
   }
+}
+
+// Functional component -- good when you don't need state
+function Image (props) {
+  return (
+    <img src={ props.url } width="250" height="250" />
+  )
 }
 
 export default FlickrSearch;
