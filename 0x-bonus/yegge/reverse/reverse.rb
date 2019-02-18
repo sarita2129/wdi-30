db3f55deca02c72f5127a9c128f19f807eb561c6
JT <certainthings@gmail.com>
2019-02-18 16:55:46 +1100

    ruby reverse for yegge

---
+++ 0x-bonus/yegge/reverse/reverse.rb
@@ -0,0 +1,23 @@
+# Takes a string and returns the reversed content of that string.
+def reverse(str)
+  output = ''
+
+  str.chars.each do |character|
+    output = character + output
+  end
+
+  output
+end
+
+# def reverse(str)
+#   # str.chars.inject { |memo, char| char + memo }
+#
+#   str.chars.inject do |memo, char|
+#     char + memo # Reassemble these backwards.
+#   end
+# end
+
+
+# Examples:
+puts reverse('hotdogs') # => 'sgodtoh'
+puts reverse(reverse('hotdogs')) # => 'hotdogs'
