33adf90453df46df24869c032cd1864c4d4d63ac
JT <certainthings@gmail.com>
2019-02-21 11:50:13 +1100

    more express examples

--- 14-node/express-intro/index.js
+++ 14-node/express-intro/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const ejs = require('ejs');
+const _ = require('underscore');
+const axios = require('axios');
 
 const app = express();
+app.use(express.static('public'));
+app.set('view-engine', 'ejs');
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Express.js');
+  const brothers = 'Groucho Harpo Chico Zeppo Gummo'.split(' ');
+  const randomBrother = _(brothers).sample();
+  res.render('home.ejs', {randomBrother: randomBrother}); // "Instance variables" -- pass data to a view
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello.ejs', {name: req.params.name});
+});
+
+app.get('/calculator/:x/:y/:operator', (req, res) => {
+  let answer;
+  if (req.params.operator === '+') {
+    answer = Number(req.params.x) + Number(req.params.y);
+  } else if (req.params.operator === '-') {
+    answer = Number(req.params.x) - Number(req.params.y);
+  }
+
+  res.render('calculator.ejs', {answer: answer});
 });
 
 app.get('/about', (req, res) => {
@@ -15,6 +38,20 @@ app.get('/info', (req, res) => {
     name: 'Groucho',
     hair: 'brown',
     luckyNumber: Math.random()
+  });
+});
+
+// You can use a regular expression instead of a string for a path.
+// I'm not sure why you would ever do this.
+app.get(/[0-9]/, (req, res) => {
+  res.send('you asked for a number');
+});
+
+app.get('/trivia', (req, res) => {
+  // fetch some trivia from http://numbersapi.com/random/trivia
+  axios.get('http://numbersapi.com/random/trivia').then((response) => {
+    // display that trivia with a view
+    res.render('trivia.ejs', {trivia: response.data});
   });
 });
 
