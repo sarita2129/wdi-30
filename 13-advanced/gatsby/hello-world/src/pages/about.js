7cb9335a9288abdab1de009c44ce757fbbf77732
JT <certainthings@gmail.com>
2019-02-22 12:47:44 +1100

    hello gatsby

---
+++ 13-advanced/gatsby/hello-world/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../components/header";
+
+export default () => (
+  <div style={{color: 'teal'}}>
+    <Header headerText="About Gatsby And Me And Our Forbidden Love" />
+    <Header headerText="This is some different text" />
+    <p>Basically get Page components for free with Gatsby</p>
+  </div>
+);
